d08eb18b1d94e3b918609e58c8e83e11a5fec7fa
rajeevanair <rajeev@Rajeevs-MacBook-Air.local>
2022-12-18 17:10:46 +0530

    1 Dec

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // http://api.weatherapi.com/v1/current.json?key=bf6abefcb90647daa11170906220512&q=London&aqi=no
 
-let place = 'Mumbai'
+
+const tempF = document.querySelector(".temperature")
+const search_field = document.querySelector(".enter_city")
+const form = document.querySelector('form')
+
+form.addEventListener('submit', searchForm)
 
 const fetchWeather = async(y) => {
 	let url = `http://api.weatherapi.com/v1/current.json?key=bf6abefcb90647daa11170906220512&q=${y}&aqi=no`
@@ -8,10 +13,23 @@ const fetchWeather = async(y) => {
 	const res = await fetch(url)
 	const data = await res.json()
 
-	console.log(y)
+	// console.log(data)
+	let temp = data.current.temp_c
+	let condition = data.current.condition.text
+	let dt = data.location.localtime
+
+	console.log(temp + " C")
+	console.log(condition)
+	console.log(dt)
 
 
 }
 
-fetchWeather(place)
+function searchForm(e){
+
+	e.preventDefault()
+	target = search_field.value
+	fetchWeather(target)
+}
+
 
